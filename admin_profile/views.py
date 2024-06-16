9f265d4411ddd7bd408db9a31fc7c5101d8ef5e9
jithu-francis <jithufrancis2000@gmail.com>
2024-06-17 01:01:14 +0530

    Formatted Students List Response.

--- admin_profile/views.py
+++ admin_profile/views.py
@@ -13,5 +13,25 @@ class StudentsListView(generics.ListAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(queryset, many=True)
-        formatted_response = {student['username']: student for student in serializer.data}
+        
+        # Constructing the desired response structure
+        formatted_response = {}
+        for student_data in serializer.data:
+            username = student_data['user']['username']
+            formatted_response[username] = {
+                **student_data['user'],  # Include all fields from the user
+                'dob': student_data['dob'],
+                'gender': student_data['gender'],
+                'city': student_data['city'],
+                'state': student_data['state'],
+                'pin_code': student_data['pin_code'],
+                'graduation_year': student_data['graduation_year'],
+                'intended_degree': student_data['intended_degree'],
+                'intended_field_of_study': student_data['intended_field_of_study'],
+                'preferred_location': student_data['preferred_location'],
+                'application_status': student_data['application_status'],
+                'comments': student_data['comments'],
+                'enrollment_date': student_data['enrollment_date']
+            }
+        
         return Response(formatted_response)
