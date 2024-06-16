dd01b65aee07e87da3ae9aecaac4233a4c9646df
jithu-francis <jithufrancis2000@gmail.com>
2024-06-17 00:08:25 +0530

    Editted Settings.

--- edu_guide/settings.py
+++ edu_guide/settings.py
@@ -140,13 +140,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# Additional locations the staticfiles app will traverse if the FileSystemFinder finder is enabled.
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR,'static')
 ]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
