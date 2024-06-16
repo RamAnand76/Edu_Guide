62c2f08a1385af5206ccc184d4adb4d6e4279fbe
jithu-francis <jithufrancis2000@gmail.com>
2024-06-17 00:50:32 +0530

    Formatted Students List Response.

--- admin_profile/serializers.py
+++ admin_profile/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from authentication.models import CustomUser, StudentsList
+from authentication.models import StudentsList, CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,7 +12,34 @@ class StudentsListSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentsList
         fields = [
-            'user', 'dob', 'gender', 'city', 'state', 'pin_code', 'graduation_year',
-            'intended_degree', 'intended_field_of_study', 'preferred_location', 
-            'application_status', 'comments', 'enrollment_date'
+            'id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',
+            'dob', 'gender', 'city', 'state', 'pin_code',
+            'graduation_year', 'intended_degree', 'intended_field_of_study',
+            'preferred_location', 'application_status', 'comments', 'enrollment_date'
         ]
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        return {
+            rep['username']: {
+                'id': rep['id'],
+                'username': rep['username'],
+                'email': rep['email'],
+                'first_name': rep['first_name'],
+                'last_name': rep['last_name'],
+                'is_staff': rep['is_staff'],
+                'is_active': rep['is_active'],
+                'dob': rep['dob'],
+                'gender': rep['gender'],
+                'city': rep['city'],
+                'state': rep['state'],
+                'pin_code': rep['pin_code'],
+                'graduation_year': rep['graduation_year'],
+                'intended_degree': rep['intended_degree'],
+                'intended_field_of_study': rep['intended_field_of_study'],
+                'preferred_location': rep['preferred_location'],
+                'application_status': rep['application_status'],
+                'comments': rep['comments'],
+                'enrollment_date': rep['enrollment_date']
+            }
+        }
--- admin_profile/views.py
+++ admin_profile/views.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from authentication.permissions import IsAdmin
 from authentication.models import StudentsList
-from admin_profile.serializers import StudentsListSerializer
+from .serializers import StudentsListSerializer
 
 class StudentsListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated, IsAdmin]
-    
     queryset = StudentsList.objects.filter(user__is_staff=False, user__is_admin=False)
     serializer_class = StudentsListSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        formatted_response = {student['username']: student for student in serializer.data}
+        return Response(formatted_response)
